77cb4017284e9a1239db529badb8cd45087ea42b
Pedro Moacyr Barcelos Neto <pedro@barcelosneto.com>
2021-03-04 05:51:57 -0400

    Adicionar arquivos de projeto.

---
+++ MyAppi/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As Informações Gerais sobre um assembly são controladas por meio do 
+' conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Verificar os valores dos atributos do conjunto
+<Assembly: AssemblyTitle("MyAppi")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MyAppi")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+<Assembly: Guid("9b763dfb-7d14-409f-b4df-e4a5fdd1723e")>
+
+' As informações de versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' É possível especificar todos os valores ou utilizar como padrão os Números da Versão e de Revisão 
+' usando o '*' como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
